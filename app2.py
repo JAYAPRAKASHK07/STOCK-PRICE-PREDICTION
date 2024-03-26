b7fe313d13c06f2a9ffa058db6013a4d53a07e2f
JAYAPRAKASHK07 <146160375+JAYAPRAKASHK07@users.noreply.github.com>
2024-03-26 07:35:57 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -10,7 +10,7 @@ import os
 import git
 
 # Specify the URL of your GitHub repository
-repo_url = 'https://github.com/JAYAPRAKASHK07/STOCK-PRICE-PREDICTION/blob/d5dcf40f0666455bf55875a365349a412cbb07e3/Stock%20Predictions%20Model%202025.keras'
+repo_url = 'https://github.com/JAYAPRAKASHK07/STOCK-PRICE-PREDICTION.git'
 
 # Define the local directory where the repository will be cloned
 repo_dir = 'Stock Predictions Model 2025.keras'
