0c2f124c2a6cc0f3abca8512889173438764eacc
JAYAPRAKASHK07 <146160375+JAYAPRAKASHK07@users.noreply.github.com>
2024-03-26 07:27:19 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -6,6 +6,18 @@ import streamlit as st
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from datetime import datetime, timedelta
+import os
+import git
+
+# Specify the URL of your GitHub repository
+repo_url = 'https://github.com/JAYAPRAKASHK07/STOCK-PRICE-PREDICTION/blob/d5dcf40f0666455bf55875a365349a412cbb07e3/Stock%20Predictions%20Model%202025.keras'
+
+# Define the local directory where the repository will be cloned
+repo_dir = 'path_to_local_directory'
+
+# Clone the repository
+if not os.path.exists(repo_dir):
+    git.Git().clone(repo_url, repo_dir)
 
 # Load the pre-trained model
 model = load_model('https://github.com/JAYAPRAKASHK07/STOCK-PRICE-PREDICTION/blob/d5dcf40f0666455bf55875a365349a412cbb07e3/Stock%20Predictions%20Model%202025.keras')
